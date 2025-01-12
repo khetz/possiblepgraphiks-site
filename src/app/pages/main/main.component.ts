f205ada56f25ab730b10e80704e592e1a3875290
Khetz Tutorials <vukheta99@gmail.com>
2025-01-12 21:44:58 +0200

    Set site up and added navbar to home page

---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from "../../sections/home/home.component";
+
+@Component({
+  selector: 'app-main',
+  imports: [HomeComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
---
+++ src/app/sections/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from "../../shared/menu/menu.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [MenuComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
