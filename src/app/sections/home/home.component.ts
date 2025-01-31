eb517740016979bb672c70c49e469e78f818c9e6
Khetz Tutorials <vukheta99@gmail.com>
2025-01-31 21:22:14 +0200

    Added new images, added home section carousel and, added gallery page

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,21 +1,44 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MenuComponent } from "../../shared/menu/menu.component";
 import * as AOS from 'aos';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuComponent],
+  imports: [MenuComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   @Output() scrollToSection: EventEmitter<string> = new EventEmitter();
 
+  images: string[] = [
+    'model.jpeg',
+    'Eni.jpg',
+    'graduate.jpg'
+  ];
+
+  currentImageIndex: number = 0;
+  intervalId: any;
+
   ngOnInit() {
     AOS.init();
+    this.startImageRotation();
+  }
+
+  startImageRotation() {
+    this.intervalId = setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    }, 2000);
   }
 
   sendMenuClickToMainComponent(sectionId: string) {
     this.scrollToSection.emit(sectionId);
+  }
+
+  ngDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 }
